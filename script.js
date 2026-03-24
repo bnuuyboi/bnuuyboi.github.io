5758f3d3623d6c520460dd23db07d8469f146750
bnuuyboi <kuejas102@gmail.com>
2026-03-24 14:27:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function sayHi() {
     alert("Hey! This works!");
 }
+// Play music when button is clicked
+const playButton = document.getElementById("playButton");
+const bgMusic = document.getElementById("bgMusic");
+
+playButton.addEventListener("click", () => {
+    bgMusic.play();
+    playButton.disabled = true; // optional: disable after clicking
+    playButton.innerText = "🎵 Playing...";
+});
