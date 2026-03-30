b1b3ccf83384678b68f1c843eb1f5ca573a49c23
bnuuyboi <kuejas102@gmail.com>
2026-03-30 09:58:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ function sayHi() {
 
 document.addEventListener("DOMContentLoaded", () => {
     const playButton = document.getElementById("playButton");
+    const pauseButton = document.getElementById("pauseButton");
     const bgMusic = document.getElementById("bgMusic");
+    const songSelector = document.getElementById("songSelector");
+    const songName = document.getElementById("songName");
+
+    // Set default song
+    bgMusic.src = songSelector.value;
+    songName.innerText = "Now Playing: " + songSelector.options[songSelector.selectedIndex].text;
 
     playButton.addEventListener("click", () => {
-        bgMusic.play().then(() => {
-            playButton.innerText = "🎵 Playing...";
-        }).catch((error) => {
-            console.log("Audio failed:", error);
-        });
+        bgMusic.play();
+    });
+
+    pauseButton.addEventListener("click", () => {
+        bgMusic.pause();
+    });
+
+    songSelector.addEventListener("change", () => {
+        bgMusic.src = songSelector.value;
+        songName.innerText = "Now Playing: " + songSelector.options[songSelector.selectedIndex].text;
+        bgMusic.play();
     });
 });
